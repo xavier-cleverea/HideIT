333d7f5671e333f233faaf704603f20a504bd989
XaviCH <xavier.caricchio@gmail.com>
2022-09-16 17:31:53 +0200

    update driver

--- driver.py
+++ driver.py
@@ -1,17 +1,37 @@
 import cv2
 import pytesseract
+from pytesseract import Output
 
 def load_img_rgb(path):
+    return cv2.imread(path)
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
 
 def load_img_data(img_rgb):
-    return pytesseract.image_to_data(img_rgb)
+    data = pytesseract.image_to_data(img_rgb,output_type=Output.DICT)
+    n = len(data['level'])
+    l = []
+    for i in range(n):
+        if data['conf'][i] > 0:
+            word = {
+                'text': data['text'][i],
+                'block': data['block_num'][i],
+                'line': data['line_num'][i],
+                'bbox': (
+                    [data['left'][i],data['top'][i]],
+                    [data['left'][i]+data['width'][i],data['top'][i]],
+                    [data['left'][i],data['top'][i]+data['height'][i]],
+                    [data['left'][i]+data['width'][i],data['top'][i]+data['height'][i]]
+                )
+            }
+            l.append(word)
+    return l
 
 def blur_img_bbox(img_rgb, bbox):
     pass
 
+# bbox = {tl - tr - bl - br}
 def paint_img_bbox(img_rgb, bbox, color):
-    pass
+    return cv2.rectangle(img_rgb,bbox[0],bbox[3],color,1)
 
 def write_text_on_img(img_rgb, bbox, font, fontsize, color):
     pass
@@ -32,7 +52,9 @@ if __name__ == "__main__":
     a <path>\tload an image
     b <text>\tblur text
     e <text>\terase text
+    m\tmark elements found
     o <old text> <new text>\toverwrite text
+    p\tshow image
     s <name>\tsave the image
     q\tquit""")
         inp = list(input().split())
@@ -40,7 +62,16 @@ if __name__ == "__main__":
             img_rgb = load_img_rgb(inp[1])
             img_data = load_img_data(img_rgb)
             print(img_data)
-        if inp[0] == 'q':
+        elif inp[0] == 'b':
+            #hay que hacer una mascara
+
+        elif inp[0] == 'm':
+            for word in img_data:
+                paint_img_bbox(img_rgb,word['bbox'],(0,0,255))
+        elif inp[0] == 'p':
+            cv2.imshow(r'Image',img_rgb)
+            cv2.waitKey(0)
+        elif inp[0] == 'q':
             break
             
 # By default OpenCV stores images in BGR format and since pytesseract assumes RGB format,
