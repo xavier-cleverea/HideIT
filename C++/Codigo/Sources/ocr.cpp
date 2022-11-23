387763d9323c733c767307df7bf4bfd91152d082
leoav99 <lleonard.avalos@gmail.com>
2022-11-23 22:08:47 +0100

    resize + grayscale

--- C++/Codigo/Sources/ocr.cpp
+++ C++/Codigo/Sources/ocr.cpp
@@ -16,12 +16,19 @@ namespace hideit {
         return nullptr;
     }
     void ocr::test() {
-        Pix *image = pixRead("/home/leo/Desktop/PAE/HideIT/C++/Test/Test-Image3.png");
-        TessApi->SetImage(image);
-        std::cout << TessApi->GetUTF8Text();
+        Pix *image = pixRead("/home/leo/Desktop/PAE/HideIT/C++/Test/Test-Image2.png");
+//      Pix *image2 = pixConvertRGBToGray(image,0.3,0.3,0.4);
+        Pix *image2 = pixScale(image,0.8,0.8);//hacer un scaling aplica grayscale al mismo tiempo.
+        TessApi->SetImage(image2);
+        TessApi->Recognize(0);
+//         auto it = TessApi->getIterator();
+        //se puede obtener un bounding box a partir del iterador
+//         std::cout << TessApi->GetUTF8Text();
         
  
         pixDestroy(&image);
+        pixDestroy(&image2);
+    
     }
 
 }
