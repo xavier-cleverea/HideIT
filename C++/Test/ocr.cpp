505780303a2dd470bcd6db02a8a32ef7ac0c2df8
leoav99 <lleonard.avalos@gmail.com>
2022-11-26 00:19:15 +0100

    implementado parte de driver.cpp

--- C++/Test/ocr.cpp
+++ C++/Test/ocr.cpp
@@ -18,13 +18,28 @@ class PARAMS {
 public:
     static Pix* image;
     static hideit::ocr ocr;
+    static hideit::driver driver;
 };
 
 hideit::ocr PARAMS::ocr = hideit::ocr();
+hideit::driver PARAMS::driver = hideit::driver();
 Pix* PARAMS::image = image_factory("Test/Resources/Test-Image2.png");
 
 void test_get_text_boxes() {
-    PARAMS::ocr.get_text_boxes(PARAMS::image);
+    tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
+    //el nivel define si se lee el texto letra por letra, palabra por palabra, linea por linea etc.
+
+    auto it = PARAMS::ocr.get_text_boxes(PARAMS::image);
+    if(it != 0){
+        while(it->Next(level)) {
+            const char* word = it->GetUTF8Text(level);
+            float conf = it->Confidence(level);
+            int x1, y1, x2, y2;
+            it->BoundingBox(level, &x1, &y1, &x2, &y2);
+//             printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n", word, conf, x1, y1, x2, y2);
+            delete[] word;
+        }
+    }
 }
 
 MAIN_TEST(test_get_text_boxes)
