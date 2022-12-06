9140c4a3bb1a7388d14cfacd6dc1db301d9591a6
Xavier <xavier.caricchio@gmail.com>
2022-12-06 19:41:51 +0100

    change

--- C++/Codigo/Headers/profile.hpp
+++ C++/Codigo/Headers/profile.hpp
@@ -1,18 +1,22 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <regex>
 
 namespace hideit {
 
     class profile 
     {
-
+        
     public:
         profile();
         ~profile();
 
-        std::string name;
         void word_ban(std::string);
         void regex_ban(std::string);
+        
+        std::string name;
+        std::vector<std::regex> regexs;
     };
 
 }
--- C++/Test/driver.cpp
+++ C++/Test/driver.cpp
@@ -1,7 +1,38 @@
 #include "test.hpp"
 
-void test_constructor() {
-    hideit::driver driver;
+Pix* image_factory(const char* source) {
+    Pix *image,*image1,*image2;
+    image = pixRead(source);
+    //image = pixScale(image,0.8,0.8);
+    //printf("%i\n",image->d);
+    image = pixConvertRGBToGray(image,0.3,0.3,0.4);
+
+    //pixDestroy(&image1);
+    //pixDestroy(&image2);
+    return image;
 }
 
-MAIN_TEST(test_constructor)
+hideit::profile profile_factory() {
+    hideit::profile profile;
+    profile.regex_ban("[0-9]+");
+    return profile;
+}
+
+class PARAMS {
+public:
+    static Pix* image;
+    static hideit::driver driver;
+    static hideit::profile profile;
+};
+
+Pix* PARAMS::image = image_factory("Test/Resources/Test-Image2.png");
+hideit::driver PARAMS::driver = hideit::driver();
+hideit::profile PARAMS::profile = profile_factory();
+
+void test_search() {
+    PARAMS::driver.search(PARAMS::profile,PARAMS::image);
+}
+
+int main() {
+    TEST(test_search)
+}
